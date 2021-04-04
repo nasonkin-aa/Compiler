3d88d37828fe4d32fd8b8e7abe39119b77bcd28b
Andrey <nasonkin-andrey@mail.ru>
2021-04-05 02:03:18 +1000

    Add Lexical Analyzer

---
+++ Main/Main/Lexem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+	public class Lexema
+	{
+		public static bool Nunber(string text)
+		{
+			int x;
+			return Int32.TryParse(text, out x);
+		}
+
+		public static bool Variable(string text)
+        {
+			bool variable = true;
+            if (text.Length == 0 || !Char.IsLetter(text[0]))
+            {
+                variable = false;
+            }
+            else
+            {
+				foreach (char i in text)
+                {
+                    if (!Char.IsDigit(i) && !Char.IsLetter(i))
+                    {
+                        variable = false;
+                    }
+                }
+            }
+            return variable;
+        }
+
+        public static bool ArOperator(string text)
+        {
+            return (text == "*" || text == "/" || text == "div" 
+                || text == "mod" || text == "and" || text == "or"
+                || text == "+" || text == "-" || text == "=" 
+                || text == "<" || text == ">" || text == "<>" 
+                || text == "<=" || text == ">=" || text == "in" || text == "not" );
+        }
+
+        public static bool ReserveWords (string text)
+        {
+            return (text == "array" || text == "var" || text == "type");
+        }
+        public static bool Comments(string text)
+        {
+            return (text == "//");
+        }
+
+
+    }
+
+}
---
+++ Main/Main/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string text = "";
+            string Namelex = "";
+
+            foreach( char i in str + "")
+            {
+                text += i;
+                if (Lexema.Nunber(text))
+                {
+                    Namelex = "Number";
+                }
+                else if (Lexema.Variable(text) && !Lexema.ArOperator(text) && !Lexema.ReserveWords(text))
+                {
+                    Namelex = "Variable";
+                }
+                else if (Lexema.ArOperator(text))
+                {
+                    Namelex = "Аrithmetic Оperator";
+                }
+                else if (Lexema.ReserveWords(text))
+                {
+                    Namelex = "Reserved Words";
+                }
+                else if (Lexema.Comments(text))
+                {
+                    Namelex = "Comments";
+                }
+                else
+                {   
+                    Namelex = "Errors";
+                }
+
+            }
+            Console.WriteLine(Namelex);
+            Console.ReadKey();
+
+        }
+    }
+}
